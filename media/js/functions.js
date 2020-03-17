36bc8702625e2e0ec34c4cb22ed5a11f52127079
lmm214 <fivemu@gmail.com>
2020-03-17 09:02:36 +0800

    update from gridea: 2020-03-17 09:02:36

--- media/js/functions.js
+++ media/js/functions.js
@@ -119,7 +119,7 @@ jQuery(document).ready(function($) {
     }
     addIcons();
     //豆瓣图书电影条目
-    $(".post-content a[href*='douban']").each(function () {
+    $(".post-content a[href*='douban.com/subject/']").each(function () {
         var _this = $(this);
         var str = _this.attr("href");
         var db_reg = /^https\:\/\/(movie|book)\.douban\.com\/subject\/([0-9]+)\/?/;
@@ -128,31 +128,54 @@ jQuery(document).ready(function($) {
             var db_id = str.replace(db_reg, "$2").toString();
             var db_api = "https://bm.weajs.com/api/";
             if (db_type == 'movie') {
-                var url = db_api + "movies/" + db_id + "/";
+              var ls_item = 'movie'+db_id;
+              var url = db_api + "movies/" + db_id + "/";
+              if(localStorage.getItem(ls_item)==null ||localStorage.getItem(ls_item)=='undefined'){
                 $.ajax({
                     url: url,
                     type: 'GET',
                     dataType: "json",
                     success: function (data) {
-                        var db_star = Math.ceil(data.rating);
-                        $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.directors + " / 类型：" + data.genres + " / " + data.pubdate + "</time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this)
+                      localStorage.setItem(ls_item,JSON.stringify(data));
+                      moiveShow(_this,ls_item)
                     }
                 });
+              }else{
+                moiveShow(_this,ls_item)
+              }
             } else if (db_type == 'book') {
-                var url = db_api + "books/" + db_id;
+              var ls_item = 'book'+db_id;
+              var url = db_api + "books/" + db_id;
+              if(localStorage.getItem(ls_item)==null ||localStorage.getItem(ls_item)=='undefined'){
                 $.ajax({
                     url: url,
                     type: 'GET',
                     dataType: 'json',
                     success: function (data) {
-                        var db_star = Math.ceil(data.rating);
-                        $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this)
+                      localStorage.setItem('book'+db_id,JSON.stringify(data));
+                      bookShow(_this,ls_item)
                     }
                 });
-            } else {
-                console.log("error")
+              }else{
+                bookShow(_this,ls_item)
+              }
             }
         }
     });
-
+    function moiveShow(_this,ls_item){
+        var storage=localStorage.getItem(ls_item);
+        var data=JSON.parse(storage);
+        var str = _this.attr("href");
+        //console.log(data)
+        var db_star = Math.ceil(data.rating);
+        $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>导演：" + data.directors + " / 类型：" + data.genres + " / " + data.pubdate + "</time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this)
+    }
+    function bookShow(_this,ls_item){
+        var storage=localStorage.getItem(ls_item);
+        var data=JSON.parse(storage);
+        var str = _this.attr("href");
+        ///console.log(data)
+        var db_star = Math.ceil(data.rating);
+        $("<div class='post-preview'><div class='post-preview--meta'><div class='post-preview--middle'><h4 class='post-preview--title'><a target='_blank' href='" + str + "'>《" + data.title + "》</a></h4><div class='rating'><div class='rating-star allstar" + db_star + "'></div><div class='rating-average'>" + data.rating + "</div></div><time class='post-preview--date'>作者：" + data.author + " </time><section style='max-height:75px;overflow:hidden;' class='post-preview--excerpt'>" + data.intro + "</section></div></div><img class='post-preview--image' src=" + data.cover + "></div>").replaceAll(_this)
+    }
 });
